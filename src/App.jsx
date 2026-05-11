56541da43b9fca24d12ca60a67779a3482349e0c
SiuzannaVach <siuzannavachaganian@gmail.com>
2026-05-11 15:12:10 +0200

    flex:  update all css imports to scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.scss";
 
 function App() {
-
   return {};
 }
 
-export default App
+export default App;
